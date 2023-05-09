ae93f3c829cb4ab022c0277535111fab7c25ee77
Parapau2 <120369026+Parapau2@users.noreply.github.com>
2023-05-09 12:38:21 +0200

    Add files via upload

--- src/N1E1/Main.java
+++ src/N1E1/Main.java
@@ -2,18 +2,14 @@ package N1E1;
 
 public class Main {
 	public static void main (String args[]) {
-		Vent instrument = new Vent ("Pere", 68);
-		
-		System.out.println("patata");
+		Vent instrument = new Vent ("flauta", 68);
 		
 		Vent.metodeStatic();
 		
-		
 		Percussio.metodeStatic();
 		
-		//Vent.metodeStatic();
-		
 		Percussio tambor = new Percussio ("Josep", 6);
+		System.out.println(tambor);
 		Percussio tamboor = new Percussio ("Joseawdawp", 6);
 		
 		
--- src/N1E2/Main.java
+++ src/N1E2/Main.java
@@ -2,7 +2,7 @@ package N1E2;
 
 public class Main {
 	public static void main(String args[]) {
-		Cotxe cotxe = new Cotxe(642);
+		Cotxe cotxe = new Cotxe(642, "Panda rojo");
 		cotxe.accelerar();
 		
 		Cotxe.frenar();
